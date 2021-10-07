61abf43d4240cb611c847fb02896c86cd2d1446a
Ackuq@ <axel@pettersson.cc>
2021-10-07 14:30:21 +0200

    Formatting

--- workers/src/main/scala/id2221/unmarshaller/SMHIProtocol.scala
+++ workers/src/main/scala/id2221/unmarshaller/SMHIProtocol.scala
@@ -8,5 +8,7 @@ object SMHIProtocol extends DefaultJsonProtocol {
   implicit val geometryFormat = jsonFormat2(SMHIResponse.Geometry);
   implicit val parameterFormat = jsonFormat5(SMHIResponse.Parameter);
   implicit val timeSerieFormat = jsonFormat2(SMHIResponse.TimeSerie);
-  implicit val forecastResponseFormat = jsonFormat4(SMHIResponse.ForecastResponse);
+  implicit val forecastResponseFormat = jsonFormat4(
+    SMHIResponse.ForecastResponse
+  );
 }
