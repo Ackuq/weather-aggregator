d381ae9d37958423abb3c5ca84ecefcbffe9a3db
ChristopherGustafson <christopher.gustafson@outlook.com>
2021-10-12 20:18:54 +0200

    Finished spark application for aggregating and sending weather data

--- spark/build.sbt
+++ spark/build.sbt
@@ -10,6 +10,7 @@ val hadoopVersion = "3.2.0"
 libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % sparkVersion,
   "org.apache.spark" %% "spark-sql" % sparkVersion,
-  "org.apache.hadoop" % "hadoop-common" % hadoopVersion,
+  "org.apache.spark" %% "spark-streaming" % sparkVersion,
+  "org.apache.spark" %% "spark-streaming-kafka-0-10" % sparkVersion,
   "id2221" %% "scala-common" % "0.1.0"
 )
--- spark/src/main/scala/id2221/Main.scala
+++ spark/src/main/scala/id2221/Main.scala
@@ -1,14 +1,16 @@
 package id2221;
 
 import org.apache.spark.sql.SparkSession;
+import id2221.common.ForecastParser;
+import id2221.tests.{ProducerTest};
+import id2221.spark.SparkConsumer;
 
 object Main {
   def main(args: Array[String]) {
-    val spark = SparkSession.builder
-      .appName("SparkApplication")
-      .config("spark.master", "local")
-      .getOrCreate;
-
-    spark.stop()
+    
+    // Start the spark consumer in a new thread (Currently to allow to also start a producer test that produces fake forecast for testing)
+    new Thread(new SparkConsumer()).start();
+    // println("Starting Producer test")
+    // new Thread(new ProducerTest()).start();
   }
 }
